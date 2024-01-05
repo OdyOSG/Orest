c9a0a8766390de18d152d5011959588b808ea357
Alex <alexander.alexeyuk@odysseusinc.com>
2024-01-05 11:22:18 +0300

    crf top 150 model

--- R/Standardization.R
+++ R/Standardization.R
@@ -4,8 +4,9 @@
 #' @return data.frame with 4 columns: nameFromListOfMeasurements, siValue, siUnit, usValue, usUnit
 #' @title Convert SI unit value to US unit value
 #' @description Function converts SI values to US
-convertSiToUs <- function(nameFromListOfMeasurements, siValue) {r <-  data.table(getConv())[Measurement == nameFromListOfMeasurements, ][, res :=  round(siValue / Conversion, 2)]
-   return(data.frame(nameFromListOfMeasurements = nameFromListOfMeasurements, siValue = siValue, siUnit = r[['SIUnit']], usValue  = r[['res']], usUnit = r[['ConventionalUnit']] ))
+convertSiToUs <- function(nameFromListOfMeasurements, siValue) {
+  r <- data.table(getConv())[Measurement == nameFromListOfMeasurements, ][, res := round(siValue / Conversion, 2)]
+  return(data.frame(nameFromListOfMeasurements = nameFromListOfMeasurements, siValue = siValue, siUnit = r[["SIUnit"]], usValue = r[["res"]], usUnit = r[["ConventionalUnit"]]))
 }
 #' @export
 #' @param nameFromListOfMeasurements one name of the measurement from list
@@ -13,8 +14,7 @@ convertSiToUs <- function(nameFromListOfMeasurements, siValue) {r <-  data.table
 #' @return data.frame with 4 columns: nameFromListOfMeasurements, siValue, siUnit, usValue, usUnit
 #' @title Convert US unit value to SI unit value
 #' @description Function converts US values to SI
-convertUsToSi <- function(nameFromListOfMeasurements, usValue) {r <- data.table(getConv())[Measurement == nameFromListOfMeasurements, ][, res :=  round(usValue * Conversion, 2)]
-  return(data.frame(nameFromListOfMeasurements = nameFromListOfMeasurements, siValue = r[['res']], siUnit = r[['SIUnit']], usValue  = usValue, usUnit = r[['ConventionalUnit']]))
+convertUsToSi <- function(nameFromListOfMeasurements, usValue) {
+  r <- data.table(getConv())[Measurement == nameFromListOfMeasurements, ][, res := round(usValue * Conversion, 2)]
+  return(data.frame(nameFromListOfMeasurements = nameFromListOfMeasurements, siValue = r[["res"]], siUnit = r[["SIUnit"]], usValue = usValue, usUnit = r[["ConventionalUnit"]]))
 }
-
-
---
+++ R/crfModel.R
@@ -0,0 +1,91 @@
+# getModel <- function() readRDS('crfModel.Rd')
+
+
+#' @importFrom data.table shift
+#' @import crfsuite
+#' @import stringr
+#' @import dplyr
+#' @importFrom data.table :=
+#' @importFrom tidytext unnest_tokens
+#' @export
+prepareTable <- function(tbl) {
+  prepTbl <- tbl %>%
+    distinct() %>%
+    mutate(
+      doc_id = row_number()
+    ) %>%
+    unnest_tokens(., token, "source_code", strip_punct = FALSE, to_lower = FALSE) %>%
+    mutate(
+      pos = case_when(
+        str_detect(token, "\\d") ~ "digits",
+        str_detect(token, "[A-Za-z]") ~ "abbr",
+        .default = "other"
+      ),
+      upperLower = ifelse(grepl("[[:upper:]]", token), "upper", "nonupper")
+    )
+  x <- data.table::as.data.table(prepTbl)
+  tbl <- x[,
+    pos_previous := shift(pos, n = 1, type = "lag"),
+    by = list(doc_id)
+  ][,
+    pos_next := shift(pos, n = 1, type = "lead"),
+    by = list(doc_id)
+  ][,
+    token_previous := shift(token, n = 1, type = "lag"),
+    by = list(doc_id)
+  ][,
+    token_next := shift(token, n = 1, type = "lead"),
+    by = list(doc_id)
+  ][,
+    pos_previous := txt_sprintf("pos[w-1]=%s", pos_previous),
+    by = list(doc_id)
+  ][,
+    pos_next := txt_sprintf("pos[w+1]=%s", pos_next),
+    by = list(doc_id)
+  ][,
+    token_previous := txt_sprintf("token[w-1]=%s", token_previous),
+    by = list(doc_id)
+  ][,
+    token_next := txt_sprintf("token[w-1]=%s", token_next),
+    by = list(doc_id)
+  ]
+  return(data.frame(x))
+}
+
+
+#' @export
+applyCrfModel <- function(preparedTable, pathToOrest) {
+
+  models <- list.files(here::here(pathToOrest, 'inst/models'), full.names = TRUE)
+  pred <- purrr::map_dfc(
+    models, ~predict(readRDS(.x),
+                      newdata = preparedTable[,
+                                              c(
+                                                "pos",
+                                                "pos_previous",
+                                                "pos_next",
+                                                "token",
+                                                "token_previous",
+                                                "token_next",
+                                                'upperLower')
+                      ],
+                      group = preparedTable$doc_id)
+  )
+
+  # model <- readRDS(pathToTheModel)
+  # pred <- predict(model,
+  #         newdata = preparedTable[,
+  #                     c(
+  #                       "pos",
+  #                       "pos_previous",
+  #                       "pos_next",
+  #                       "token",
+  #                       "token_previous",
+  #                       "token_next",
+  #                       'upperLower')
+  #                     ],
+  #         group = preparedTable$doc_id)
+  return(pred)
+}
+
+
